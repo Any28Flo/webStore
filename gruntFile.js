dcb33e002bc39f9236d1a1ce6d47a33b543503a2
Analin Flores <foglzerika@gmail.com>
2017-10-13 15:39:22 -0500

    Making change in list on css file

--- gruntFile.js
+++ gruntFile.js
@@ -1,26 +1,40 @@
-module.exports = function(grunt){
+const gulp = require('gulp');
+const sass= require('gulp-sass');
+gulp.registerTask('sass', ()=>{
+  return gulp.src('sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('build'))
+})
+module.exports = (grunt)=>{
 // grunt.registerTask('speak',function(){
 //   console.log("Hello from grunt");
 // })
 // Project configuration.
+
 grunt.initConfig({
   concat: {
     js: {
       src: ['js/app.js', 'js/functions.js'],
-      dest: 'build/js/app.min.js',
+      dest: 'build/js/app.min.js'
     },
   },
-  uglify: {
-    my_target: {
-      files: {
-        'build/js/appMain.js': ['js/functions']
-      }
-    }
-  }
+  watch: {
+  scripts: {
+    files: ['js/*.js'],
+    tasks: ['concat']
+
+  },
+},
+  // uglify: {
+  //   my_target: {
+  //     files: {
+  //       'build/js/output.min.js': ['js/functions.js']
+  //     }
+  //   }
+  // }
 });
 grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-uglify');
-
-
+grunt.loadNpmTasks('grunt-contrib-watch');
+gulp.task('default',['sass']);
 };
